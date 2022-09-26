02224c77d75f7b0a83e7911b6fa145c2d9e28ad0
vishalvishalgupta <vishal.vishal.gupta48@gmail.com>
2022-09-27 00:36:00 +0530

    Update getGifs.js

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
 
-  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=Y13YNSxyHCUX3xAYuPwAORkqaVmJhtz1`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=fLSS6qNmoD8qptXUoZonH37XjK9Y5umf`;
 
   const resp = await fetch(url);
   const { data } = await resp.json();  
