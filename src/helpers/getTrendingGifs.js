5f11515388bc5af7b3ee865be07f9b44f4575198
vishalvishalgupta <vishal.vishal.gupta48@gmail.com>
2022-09-27 12:08:15 +0530

    Add files via upload

---
+++ src/helpers/getTrendingGifs.js
@@ -0,0 +1,20 @@
+export const getTrendingGifs = async (limit) => {
+
+    const url = `https://api.giphy.com/v1/gifs/trending?limit=${limit}&api_key=fLSS6qNmoD8qptXUoZonH37XjK9Y5umf`;
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    console.log(data);
+
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    })
+    )
+
+    return gifs;
+
+
+}
