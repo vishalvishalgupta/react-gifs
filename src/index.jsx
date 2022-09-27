27d1c5584678cd8769f548c19c3ad44470536ac7
vishalvishalgupta <vishal.vishal.gupta48@gmail.com>
2022-09-27 12:06:33 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { GifSearchApp } from './GifSearchApp';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GifSearchApp } from "./GifSearchApp";
+import { TrendingGifGrid } from "../src/components/TrendingGifs";
 
-ReactDOM.render( <GifSearchApp />,document.getElementById('root'));
-
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+        <Route exact path="/" element={<GifSearchApp />} />
+        <Route exact path="/trending-gifs" element={<TrendingGifGrid />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
